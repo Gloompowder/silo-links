939babdc92712b5ce900ae879720c40db5e03ad1
William  Lin <williamlin@Williams-MacBook-Pro.local>
2022-05-28 19:17:19 -0400

    save

--- src/Applink.js
+++ src/Applink.js
@@ -15,10 +15,10 @@ function Applink() {
               <div className="logo-div">
       <img onClick = {handleClickhome} src={logo} alt="Silo-logo" className = "logo"/>
       </div>
-                    <a className = 'appstorelink' href='https://apps.apple.com/us/app/silo-pods/id1604451202'>
+                    <a className = 'appstorelink' target= '_blank' href='https://apps.apple.com/us/app/silo-pods/id1604451202'>
                 <img className = 'appstore' alt='Get Silo on the App Store' src={Appstore}/>
             </a>
-            <a className = 'googleplaylink' href='https://play.google.com/store/apps/details?id=com.silo&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+            <a className = 'googleplaylink' target= '_blank' href='https://play.google.com/store/apps/details?id=com.silo&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
                 <img className = 'googleplay' alt='Get Silo on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
             </a>
     </div>
